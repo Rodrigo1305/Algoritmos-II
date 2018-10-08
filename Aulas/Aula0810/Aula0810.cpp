3d911933fa1ad406a18f8aa6029f0df95787ae43
Rodrigo1305 <rlima3170@live.com>
2018-10-08 09:21:16 -0300

    Criando projeto aula 0810

---
+++ Aulas/Aula0810/Aula0810.cpp
@@ -0,0 +1,24 @@
+// Aula0810.cpp : Defines the entry point for the console application.
+//
+
+
+/* comentario longo , bloco 
+(mais de uma linha ) local para fazer comentarios longos
+
+*/
+
+
+#include "stdafx.h"             // inclusão de biblioteca // sempre nomear as variaveis... que colocar
+// C ANSI 
+#include <stdio.h>  
+#include <stdlib.h>           // STD = standart bibliteca padrão
+
+
+
+
+
+int main() // função obrigatório nunca apagar
+{
+    return 0;
+}
+
