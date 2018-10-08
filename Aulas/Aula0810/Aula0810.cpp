9515d1f00d593f647ed705364670b36025517bdc
Rodrigo1305 <rlima3170@live.com>
2018-10-08 09:40:52 -0300

    Varias alterações  no código

--- Aulas/Aula0810/Aula0810.cpp
+++ Aulas/Aula0810/Aula0810.cpp
@@ -13,12 +13,19 @@
 #include <stdio.h>  
 #include <stdlib.h>           // STD = standart bibliteca padrão
 
-
+// VARIAVEIS IMPORTANTES ( GLOBAIS , VISIVEIS EM TODO CÓDIGO )
+int a;      // inteiro  numeros reais sem virgula ou ponto
+float b;    // real ( ponto fluente) numeros com vigulas  padrão Americano utilizar ( Ponto . )      
+double c;   // real ( maior precisão) 
+bool d;     // booleano ( verdadeiro / falso ) ( true/false)
 
 
 
 int main() // função obrigatório nunca apagar
 {
-    return 0;
+	// variavel local ( visivel somente nesta função, a partir desta linha)
+	int e; 
+    
+	return 0;
 }
 
