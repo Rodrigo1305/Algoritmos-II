b7ed46b7617736cdfab49ba7c5979e35695cfb70
Rodrigo1305 <rlima3170@live.com>
2018-10-10 11:09:46 -0300

    Codigo de aula 10-10

--- Aula1010/Aula1010/Aula1010.cpp
+++ Aula1010/Aula1010/Aula1010.cpp
@@ -2,10 +2,43 @@
 //
 
 #include "stdafx.h"
+#include <stdlib.h>
 
+void HelloWorld()
+
+
+{
+	
+	printf("\t\t\t\t\t\t\tHello World!\n");                /* BARRA T É PARA DAR ESPAÇO*/
+}
+int Soma2Valores(int a, int b)
+{
+			/*int c;
+	c = a + b;
+	return c;*/
+	return a + b;
+}
+int LerValorInteiro()
+{
+	int variavel;
+	printf("\nFavor digitar um numero inteiro:");
+	scanf_s("%i", &variavel);
+	return variavel;
+}
 
 int main()
 {
+
+
+	HelloWorld();
+	int primeiroValor = LerValorInteiro();
+	int segundovalor = LerValorInteiro();
+	
+	int d = Soma2Valores(99, 1);
+	printf("\t\t\t\t\t\tResultado:%i\n", d);
+
+
+	system("pause");
     return 0;
 }
 
