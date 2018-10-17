6bfbc7af18cf9b67aa641d92c22add624548c28c
caiocandidosilveira <caiocandidosilveira@outlook.com>
2018-10-17 11:24:23 -0300

    Aula 17-10-2018

---
+++ Aula1710/Aula1710/Aula1710.cpp
@@ -0,0 +1,49 @@
+// Aula1710.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	int a = 5;
+	
+	if (true)
+	{
+		printf("imprima essa frase\n");
+// sempre ira imprimir , pois o resultado da impressão no if eh verdadeiro
+	}
+
+	if (false)
+{
+		printf("Essa frase nunca sera impressa\n");
+// resultado da expressão eh falsa, nunca ira utilizar este bloco
+	}
+	int(a == 5)
+
+		if (a == 3)
+
+	{
+		printf("A eh igual a 3");
+	}
+
+if (a != 3)
+	{
+		printf("a e diferente de 3");
+	}
+
+if (a <= 5) // if (a < 3 )( -1000 , 0 ,2 ,4 )  tem que ter noção do que ta fazendo
+{
+	printf)("a eh menor ou igual a 5\n");
+}
+
+if (a < 5)
+{
+	printf)("A Eh menor que 5\n");
+}
+
+return 0;
+}
+
+
+
